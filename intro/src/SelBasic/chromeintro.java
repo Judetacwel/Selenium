484ea6c5e09314320bb2052c51d54a46c39f72f2
Jude Tacwel <judetacwel007@gmail.com>
2022-10-13 12:36:49 -0400

    first commit

--- intro/src/SelBasic/chromeintro.java
+++ intro/src/SelBasic/chromeintro.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class chromeintro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\judet\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 
