c55f6007d50eefa3fa55442eebf2f7e356f0b673
Jude Tacwel <judetacwel007@gmail.com>
2022-10-13 20:21:41 -0400

    second commit

--- intro/src/SelBasic/chromeintro.java
+++ intro/src/SelBasic/chromeintro.java
@@ -10,6 +10,10 @@ public class chromeintro {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\judet\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
+		driver.quit();
 		
 
 	}
