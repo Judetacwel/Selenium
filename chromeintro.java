e40e44791d41f2d7308bb6a51e7e512fd575664f
Jude Tacwel <59969373+Judetacwel@users.noreply.github.com>
2022-10-18 18:26:16 -0400

    Add files via upload

---
+++ chromeintro.java
@@ -0,0 +1,15 @@
+package SelBasic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class chromeintro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\judet\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
