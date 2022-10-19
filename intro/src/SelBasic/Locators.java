5a715c446c1f749b03ed9ebb5ab51963d8b31d24
Jude Tacwel <judetacwel007@gmail.com>
2022-10-19 01:25:11 -0400

    Third commit Css Locators

--- intro/src/SelBasic/Locators.java
+++ intro/src/SelBasic/Locators.java
@@ -1,5 +1,7 @@
 package SelBasic;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,10 +13,16 @@ public class Locators {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\judet\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
+		//implicit wait - 5 seconds timeout
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("Jude");
 		driver.findElement(By.name("inputPassword")).sendKeys("judetacwel0");
 		driver.findElement(By.className("signInBtn")).click();
+		
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 
 	}
 
